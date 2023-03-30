d1d62087a21bfab3a3940cfe6f7b606b10cbccb9
nikstormCh <116068053+nikstormCh@users.noreply.github.com>
2023-03-30 12:39:56 +0300

    time 12:39

--- Checkers.java
+++ Checkers.java
@@ -1,9 +1,9 @@
 import java.util.ArrayList;
 
 public class Checkers {
-    private int wordLength = 5;
+    private int wordLength = 5; // sõna pikkus
 
-    public boolean wordLengthChecker(int wordLength){
+    public boolean wordLengthChecker(int wordLength){// kontrollib, kas sõna on 5 tähe pikkusega
 
         if (this.wordLength > wordLength){
 
@@ -20,7 +20,7 @@ public class Checkers {
         return true;
     }
 
-    public boolean wordInList(StringBuilder sona, ArrayList<String> massiiv){
+    public boolean wordInList(StringBuilder sona, ArrayList<String> massiiv){ // kontrollib kas on selline sõna meie sõnade loetelus
 
         if (massiiv.contains(String.valueOf(sona))){
             return false;
@@ -30,6 +30,7 @@ public class Checkers {
     }
 
     public ArrayList<Integer> greenLetter(StringBuilder user,StringBuilder random){
+        //kontrollib kas täht on õige ja õiges kohas, kasutades indeksid. Tagastab indeksite massiiv
 
         ArrayList<Integer> result = new ArrayList<>();
 
@@ -44,12 +45,12 @@ public class Checkers {
     }
 
     public ArrayList<Integer> yellowLetter(StringBuilder user,StringBuilder random){
-
+        //kontrollib, kas täht on õige, kasutades indeksid. Tagastab indeksite massiiv
         ArrayList<Integer> result = new ArrayList<>();
 
         for (int i = 0; i < user.length(); i++) {
 
-            if(random.indexOf(String.valueOf(user.charAt(i)))!=-1){
+            if(random.indexOf(String.valueOf(user.charAt(i)))!=-1){ //kui sõnas tähte pole siis meetod "indexOf" tagastab -1
                 result.add(random.indexOf(String.valueOf(user.charAt(i))));
             }
 
--- GameProcess.java
+++ GameProcess.java
@@ -1,29 +1,29 @@
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
 
 public class GameProcess {
-    private int tries = 6;
-    private static final String GREEN = "\u001B[32m";
-    private static final String RESET = "\u001B[0m";
-    private static final String YELLOW = "\u001B[33m";
-    private static final String BLACK = "\u001B[30m";
+    private int tries = 6; //katsete arv
+    private static final String GREEN = "\u001B[32m"; //konsooli teksti värvi muutumiseks
+    private static final String RESET = "\u001B[0m"; //konsooli teksti värvi muutumiseks
+    private static final String YELLOW = "\u001B[33m"; //konsooli teksti värvi muutumiseks
+    private static final String BLACK = "\u001B[30m"; //konsooli teksti värvi muutumiseks
 
     public void Game(StringBuilder randomWord){
 
-        Text text = new Text();
-        Checkers check = new Checkers();
-        ListAndFile laf = new ListAndFile();
-        boolean result = false;
-        Alphabet alphabet = new Alphabet();
-        String userInput = "_ _ _ _ _";
+        Text text = new Text(); // klass, kust me saame kõik sõnumid
+        Checkers check = new Checkers(); // klass, mis kontrollib mingeid tegevusi
+        ListAndFile laf = new ListAndFile(); // kontrollimiseks
+        boolean result = false; // mängu resultaati leidmiseks
+        Alphabet alphabet = new Alphabet(); // klass, kus on kõik tähistikud
+        String userInput = "_ _ _ _ _"; //algne mängija vastus(tühi)
 
         System.out.println("Let the game begin!");
-        while (tries!=0) {
+        while (tries!=0) {//kontrollib kas on veel katseid mängimiseks
 
-            // Loome tähte erinevaid värvidega
+
+            // Loome tähistikud erinevaid värvidega, selleks et tulevikus väljastada ekraanile tähtede informatsiooni
 
             StringBuilder alphabetGreen = alphabet.getGreen();
             StringBuilder alphabetGrey = alphabet.getGrey();
@@ -33,7 +33,7 @@ public class GameProcess {
 
             System.out.println(text.numberOfTries(tries)); // Kirjutame katsete numbrit
 
-            text.remainingLettersAlphabet(alphabetGrey); // Kirjutame ülejaanud tähed
+            text.remainingLettersAlphabet(alphabetGrey); // Kirjutame tähed, mis me saame kasutada
 
             if (alphabetGreen.length()!=0){
 
@@ -48,57 +48,61 @@ public class GameProcess {
 
             if (alphabetUsed.length()!=0){
 
-                text.incorrectLetters(alphabetUsed,BLACK,RESET);
+                text.incorrectLetters(alphabetUsed,BLACK,RESET); // kutsume meetodi, mis kirjutab, millised tähed on valed
 
 
             }
 
             System.out.print("Kirjutage oma sõna: ");
-            System.out.println(userInput + "\n" + text.manyDashes());
+
+            System.out.println(userInput + "\n" + text.manyDashes()); // väljastatakse ekraanile kotnrollitud tähed sõnas
 
             Scanner sc = new Scanner(System.in); // Palume, et inimene kirjutaks sõna
 
             StringBuilder answer = new StringBuilder(sc.nextLine());
 
-            if (!check.wordLengthChecker(answer.length())) continue;// Kui sõna pikkkus on suurem kui 5, siis see on vale, ja me läheme tagasi
+            if (!check.wordLengthChecker(answer.length())) continue;// Kui sõna pikkkus on suurem või väiksem kui 5, siis see on vale, ja me läheme tagasi
 
             if(check.wordInList(answer, laf.getSonad())) continue;// Samalt kui sõna puudub failiListis, siis sama tagasi
 
             if(answer.toString().equals(randomWord.toString())) {// Kui sõna on see, mida palutati, siis läheme tsükklist välja
-                result = true;
+                result = true; //muutume resultaat, anname talle väärtus, mis näitab võitmist
                 break;
             }
 
-            ArrayList<Integer> greenIndex = check.greenLetter(answer, randomWord); //Õige sõna tähed (rohelise värviga)
-            alphabet.add_Green_Remove_Grey(answer, greenIndex); // Asendame tähtedeListi värvi rohelisega
+            ArrayList<Integer> greenIndex = check.greenLetter(answer, randomWord);
+            //Õige sõna tähtede indeksid (rohelise värviga), indeksid saab kasutada nii peidetud sõnale, kui ka mängija sõnale
+            alphabet.add_Green_Remove_Grey(answer, greenIndex); // Muudame tähestikud, tähestiku klassis
 
-            ArrayList<Integer> yellowIndex = check.yellowLetter(answer, randomWord);//Üige süna tähed (kollase värviga)
-            yellowIndex.removeAll(greenIndex);//Asendame tähtedeListi värvi kollasega
+            ArrayList<Integer> yellowIndex = check.yellowLetter(answer, randomWord);
+            //Õige tähe vale kohas indeksid, indeksid saab kasutada ainult PEIDETUD sõnas(kollase värviga)
+            yellowIndex.removeAll(greenIndex);//Paneme kollast indeksist ära rohelised indeksid
 
             ArrayList<Integer> yellowIndexAlphabet = new ArrayList<>();
+            //Õige tähe vale kohas indeksid, indeksid saab kasutada ainult MÄNGIJA sõnas(kollase värviga)
 
             userInput ="";
 
-            x:for (int i = 0; i < 5; i++) {
+            x:for (int i = 0; i < 5; i++) { //x on selleks et liikuda tsükli alguse
 
                 if(greenIndex.size()!=0){
 
                     if (yellowIndex.size()==0){
 
-                        for (int j = 0; j < greenIndex.size(); j++) {
+                        for (int j = 0; j < greenIndex.size(); j++) { //võtab kontrollimiseks kõik väärtused greenIndekx listist
 
-                            if (greenIndex.get(j) == i){
-                                userInput+= GREEN + answer.charAt(greenIndex.get(j)) + RESET + " ";
-                                continue x;
+                            if (greenIndex.get(j) == i){ //kontrollib indeksid mängija sõnas ja for tsükli muutuja (mis on 0-st 4-ni)
+                                userInput+= GREEN + answer.charAt(greenIndex.get(j)) + RESET + " "; //userinputile pannatakse roheline täht
+                                continue x; //tuleme tsükli alguse
                             }
 
                         }
 
                     }else {
 
-                        for (int j = 0; j < greenIndex.size(); j++) {
+                        for (int j = 0; j < greenIndex.size(); j++) {//võtab kontrollimiseks kõik väärtused greenIndekx listist
 
-                            for (int k = 0; k < yellowIndex.size(); k++) {
+                            for (int k = 0; k < yellowIndex.size(); k++) {//võtab kontrollimiseks kõik väärtused yellowIndekx listist
 
                                 if (greenIndex.get(j) == i){
                                     userInput+= GREEN + answer.charAt(greenIndex.get(j)) + RESET + " ";
@@ -106,9 +110,11 @@ public class GameProcess {
 
                                 }
                                 else if (answer.indexOf(String.valueOf(randomWord.charAt(yellowIndex.get(k)))) == i) {
+                                    //võtab väärtus yellowIndex listist, vaatab mis täht(märk) on see peidetus sõnas, muutub märk sõnaks,
+                                    //vaatab, mis indeksis on sama täht mängija sõnas
 
-                                    userInput+= YELLOW + randomWord.charAt(yellowIndex.get(k)) + RESET + " ";
-                                    yellowIndexAlphabet.add(i);
+                                    userInput+= YELLOW + randomWord.charAt(yellowIndex.get(k)) + RESET + " ";//userinputile pannatakse kollane täht
+                                    yellowIndexAlphabet.add(i); //lisatakse indeks listile, mis vaatab indeksid MÄNGIJA sõnas
 
                                     continue x;
                                 }
@@ -118,7 +124,7 @@ public class GameProcess {
 
                     }
 
-                } else if (yellowIndex.size()!=0) {
+                } else if (yellowIndex.size()!=0) {//see koodi lõik on vajalik, kui rohelineIndex on tühi aga kollaneIndex mitte
 
                     for (int k = 0; k < yellowIndex.size(); k++) {
 
@@ -126,19 +132,22 @@ public class GameProcess {
 
                             userInput += YELLOW + randomWord.charAt(yellowIndex.get(k)) + RESET + " ";
                             yellowIndexAlphabet.add(i);
-                            continue x;
+                            continue x;//sama printsiib mis on üleval
                         }
                     }
                 }
-                userInput+="_ ";
+                userInput+="_ ";//kui midagi ei sobinud siis lisatakse userInputile tühik ("_  ")
             }
             alphabet.add_Yellow_Remove_Grey(answer, yellowIndexAlphabet);
+            //lisatakse tähed kollase tähestikuse kasutades mängija sõnu ja indeksid, mis sobivad mängija sõnale
 
-            ArrayList<Integer> usedIndex = new ArrayList<>(Arrays.asList(0,1,2,3,4));
+            ArrayList<Integer> usedIndex = new ArrayList<>(Arrays.asList(0,1,2,3,4)); // list, selleks et leida kõik valed tähed
             usedIndex.removeAll(greenIndex);
-            usedIndex.removeAll(yellowIndexAlphabet);
+            usedIndex.removeAll(yellowIndexAlphabet);//kustutakse indeksid, kus on õiged tähed, sellega ülejäänud indeksid mängija sõnas on valed
 
-            for (int i = 0; i < usedIndex.size(); i++) {
+            for (int i = 0; i < usedIndex.size(); i++) {//kontroll, mis kontrollib kas sõnas on samad tähed,
+                //Näiteks sõna: beers, ütleme, et esimene "e" on õiges kohas, aga teine ei ole, siis me paneme e rohelise sõnastikuse
+                //ja me ei saa panna teine "e" used sõnastikuse, sest see ei ole tehniliselt vale täht
 
                 for (int j = 0; j < greenIndex.size(); j++) {
                     if (answer.charAt(usedIndex.get(i)) == answer.charAt(greenIndex.get(j))){
@@ -147,7 +156,7 @@ public class GameProcess {
                 }
 
             }
-            for (int i = 0; i < usedIndex.size(); i++) {
+            for (int i = 0; i < usedIndex.size(); i++) {//sama printsiib mis oli üleval
                 for (int j = 0; j < yellowIndexAlphabet.size(); j++) {
                     if (answer.charAt(usedIndex.get(i)) == answer.charAt(yellowIndexAlphabet.get(j))){
                         usedIndex.remove(i);
@@ -157,9 +166,9 @@ public class GameProcess {
 
             alphabet.add_Used_Remove_Grey(answer, usedIndex);
 
-            tries--;
+            tries--; // tsükli lõpp, võtab üks katse maha
         }
-        if (result){
+        if (result){ //vaatab resultaati väärtust(true = võit, false = kaotus)
             System.out.println(text.winningSpeech());
         }else {
             System.out.println(text.loserSpeech());
